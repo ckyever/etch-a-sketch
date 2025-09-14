8b1d5e0990d1f411f8e353f08eb384884f1d7df9
ckyever <ckyeverg@gmail.com>
2025-09-14 14:49:55 +1000

    Refactor logic into a function to create empty grid

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-const GRID_DIMENSIONS = 16;
+const DEFAULT_GRID_DIMENSIONS = 16;
 const etchASketch = document.querySelector("#etch-a-sketch");
 
-for (let i = 0; i < GRID_DIMENSIONS; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    etchASketch.appendChild(row);
+function createEmptyGrid(dimensions) {
+    for (let i = 0; i < dimensions; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        etchASketch.appendChild(row);
 
-    for (let i = 0; i < GRID_DIMENSIONS; i++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.addEventListener("mouseenter", () => {
-            cell.style.backgroundColor = "black";
-            console.log("touched a div");
-        });
-        row.appendChild(cell);
+        for (let i = 0; i < dimensions; i++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.addEventListener("mouseenter", () => {
+                cell.style.backgroundColor = "black";
+                console.log("touched a div");
+            });
+            row.appendChild(cell);
+        }
     }
 }
+
+createEmptyGrid(DEFAULT_GRID_DIMENSIONS);
