122c8cecbd75dc28226107be0f0ca72fd1a3a3ce
ckyever <ckyeverg@gmail.com>
2025-09-14 14:46:43 +1000

    Fix hover effect, it should be drawing

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ for (let i = 0; i < GRID_DIMENSIONS; i++) {
     for (let i = 0; i < GRID_DIMENSIONS; i++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
+        cell.addEventListener("mouseenter", () => {
+            cell.style.backgroundColor = "black";
+            console.log("touched a div");
+        });
         row.appendChild(cell);
     }
 }
