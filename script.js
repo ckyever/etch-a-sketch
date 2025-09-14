a2f1e63c40170b67704e9f4b4a6dfab0a7d0a00b
ckyever <ckyeverg@gmail.com>
2025-09-14 14:29:22 +1000

    Add grid of divs

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const GRID_DIMENSIONS = 4;
+const etchASketch = document.querySelector("#etch-a-sketch");
+
+for (let i = 0; i < GRID_DIMENSIONS; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    etchASketch.appendChild(row);
+
+    for (let i = 0; i < GRID_DIMENSIONS; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        row.appendChild(cell);
+    }
+}
