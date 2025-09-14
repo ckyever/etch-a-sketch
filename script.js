bf3c8eb3317e97502533c6c2cfe499f4558a7281
ckyever <ckyeverg@gmail.com>
2025-09-14 17:27:41 +1000

    Implement randomness and darkening effect when sketching

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const DEFAULT_GRID_DIMENSIONS = 16;
 const MAX_GRID_DIMENSIONS = 100;
+const INITIAL_OPACITY_RANGE = 0.4;
+const OPACITY_INCREMENT = 0.1;
+
 const etchASketch = document.querySelector("#etch-a-sketch");
 
 function createEmptyGrid(dimensions) {
@@ -12,8 +15,13 @@ function createEmptyGrid(dimensions) {
         for (let i = 0; i < dimensions; i++) {
             const cell = document.createElement("div");
             cell.classList.add("cell");
+            cell.style.backgroundColor = "black";
+            cell.style.opacity = "0";
             cell.addEventListener("mouseenter", () => {
-                cell.style.backgroundColor = "black";
+                if (cell.style.opacity === "0") {
+                    cell.style.opacity = Math.random() * INITIAL_OPACITY_RANGE;
+                }
+                cell.style.opacity = Number(cell.style.opacity) + OPACITY_INCREMENT;
             });
             row.appendChild(cell);
         }
